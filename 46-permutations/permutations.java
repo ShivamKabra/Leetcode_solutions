e3837ad9240a4c08cb0660e32fdf5784fac896f8
Shivam Kabra <64693133+ShivamKabra@users.noreply.github.com>
2023-07-21 22:33:19 +0530

    Time: 2 ms (48.17%) | Memory: 43.7 MB (85.40%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public void dfs(List<List<Integer>> ans, List<Integer> ds, int[] nums, boolean[] freq){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i] = true;
+                ds.add(nums[i]);
+                dfs(ans, ds, nums, freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
+
+        return;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+
+        dfs(ans, ds, nums, freq);
+
+        return ans;
+    }
+}
