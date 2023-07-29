7a3c9bbfc402031dd4c91ad3231218b3bb8426a2
Shivam Kabra <64693133+ShivamKabra@users.noreply.github.com>
2023-07-29 12:35:19 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (85.50%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -7,12 +7,13 @@ class Solution {
             if(nums[i]==1){
                 count++;
             }else{
+                max = Math.max(max, count);
                 count=0;
             }
 
-            max = Math.max(max, count);
+            
         }
 
-        return max;
+        return Math.max(count, max);
     }
 }
