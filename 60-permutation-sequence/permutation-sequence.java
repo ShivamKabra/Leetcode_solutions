69fbd3078d7f75493515ccc154091395a0d5fedf
Shivam Kabra <64693133+ShivamKabra@users.noreply.github.com>
2023-08-20 11:46:12 +0530

    Time: 1539 ms (5.02%) | Memory: 126.7 MB (5.02%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,52 @@
+class ListComparator implements Comparator<List<Integer>> {
+    @Override
+    public int compare(List<Integer> list1, List<Integer> list2) {
+        // Compare the first element of each list
+        return list1.get(0).compareTo(list2.get(0));
+    }
+}
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = i+1;
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[n];
+        helper(ans, ds, freq, arr);
+
+       Collections.sort(ans, new ListComparator());
+        StringBuilder sb = new StringBuilder();
+
+        List<Integer> temp  = ans.get(k-1);
+
+        for(int ele : temp){
+            sb.append((char)(ele+'0'));
+        }
+
+        return sb.toString();
+    }
+
+    public void helper(List<List<Integer>> ans, List<Integer> ds, boolean[] freq, int[] arr){
+        if(ds.size()==arr.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(arr[i]);
+                helper(ans, ds, freq, arr);
+                ds.remove(ds.size()-1);
+                freq[i] = false;
+            }
+            
+        }
+        return;
+    }
+}
