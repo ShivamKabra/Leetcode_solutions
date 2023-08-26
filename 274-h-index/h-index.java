9acf772d87c96d7c0ac754773d68a48badee915d
Shivam Kabra <64693133+ShivamKabra@users.noreply.github.com>
2023-08-26 14:20:12 +0530

    Time: 2 ms (64.57%) | Memory: 40.4 MB (65.74%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int low  = 0;
+        int high = n-1;
+
+
+        int mid;
+
+        while(high>=low){
+            mid = low + (high-low)/2;
+
+            if(citations[mid]==n-mid){
+                return citations[mid];
+            }else if(citations[mid]>n-mid){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+
+        return n-low;
+    }
+}
