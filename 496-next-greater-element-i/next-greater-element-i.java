4fdd018a5dec33a5b85370db5750d2ab6027fba8
Shivam Kabra <64693133+ShivamKabra@users.noreply.github.com>
2023-10-11 20:28:26 +0530

    Time: 4 ms (59.39%) | Memory: 43.5 MB (39.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        int j;
-        Arrays.fill(ans, -1);
-        for(int i=0;i<nums1.length;i++){
-            boolean found=false;
-           for( j=0;j<nums2.length;j++){
-               if(nums2[j]>nums1[i] && found){
-                   ans[i]=nums2[j];
-                   break;
-               }
+        int[] val = find_next_greater(nums2);
 
-               if(nums2[j]==nums1[i]){
-                   found=true;
-               }
-           }
-           if(j==nums2.length){
-               ans[i]=-1;
-           }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            map.put(nums2[i], val[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+
+    public int[] find_next_greater(int[] nums){
+        int[] ans = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = nums.length-1; i>=0;i--){
+            while(st.size()>0 && st.peek()< nums[i]){
+                st.pop();
+            }
+
+            if(st.size()==0){
+                ans[i]=-1;
+            }else{
+                ans[i]=st.peek();
+            }
+
+            st.push(nums[i]);
         }
 
         return ans;
